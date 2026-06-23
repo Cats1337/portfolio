4334bf5a936f7a2d4f55cb76879a1b9db8ab2582
Cats <42129397+Cats1337@users.noreply.github.com>
2026-06-23 00:52:42 -0400

    Updates

---
+++ assets/ExportJSONPluginInstance-CC3a1qso.js
@@ -0,0 +1,1 @@
+var e=2,t=class{#e;constructor(e){this.#e=e}async export(e){let t={supported:!1};switch(e){case`json`:t.supported=!0,t.blob=await this.#t();break}return t}#t=async()=>{let t=JSON.stringify(this.#e.actualOptions,(e,t)=>{if(!e.startsWith(`_`))return t},e);return Promise.resolve(new Blob([t],{type:`application/json`}))}};export{t as ExportJSONPluginInstance};
